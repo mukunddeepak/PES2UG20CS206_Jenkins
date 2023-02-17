be7966bc74552fad5dfe51d7e0ece31c0c696d42
Mukund <2255mukund@gmail.com>
2023-02-17 08:42:05 +0530

    First Trigger

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,4 +1,4 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "This is to trigger an event in the repo\n";
   return 0;
 }
